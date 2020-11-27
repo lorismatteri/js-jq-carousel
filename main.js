1508b9ddf2d2fd7968548ba4c50d64ca18acfe40
Loris Matteri <loris.matteri93@gmail.com>
2020-11-27 18:24:26 +0100

    Carosello in dietro
    stessa cosa nel senso opposto

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
+//Generare un carosello di immagini
+
+//1. Prendo le icone delle frecce con jQuery e le metto in una variabile
+
 var avanti = $('.next i');
 
 var indietro = $('.prev i');
+
+//Generiamo un carosello in avanti spostando la classe active da una img all'altra
 
 avanti.click(function() {
     var imgVisualizzata = $('img.active');
@@ -11,6 +17,21 @@ avanti.click(function() {
 
     if(imgVisualizzata.hasClass('last')) {
         $('img.first').addClass('active');
+    }
+    
+});
+
+//Ora facciamo in senso opposto
+
+indietro.click(function() {
+    var imgVisualizzata = $('img.active');
+
+    imgVisualizzata.removeClass('active');
+
+    imgVisualizzata.prev().addClass('active');
+
+    if(imgVisualizzata.hasClass('first')) {
+        $('img.last').addClass('active');
     }
     
 });
