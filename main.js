c6bc12aa7a641e1e81f7ce74fdec1e1e82e3c65b
Loris Matteri <loris.matteri93@gmail.com>
2020-11-27 18:07:41 +0100

    carosello in avanti
    con if costruito un carosello "infinito" in avanti

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-console.log("ciaone");
+var avanti = $('.next i');
+
+var indietro = $('.prev i');
+
+avanti.click(function() {
+    var imgVisualizzata = $('img.active');
+
+    imgVisualizzata.removeClass('active');
+
+    imgVisualizzata.next().addClass('active');
+
+    if(imgVisualizzata.hasClass('last')) {
+        $('img.first').addClass('active');
+    }
+    
+});
