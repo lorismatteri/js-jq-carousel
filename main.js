d74d145f22df4f88c71d0877cbb7d99551d9813e
Loris Matteri <loris.matteri93@gmail.com>
2020-11-29 12:39:31 +0100

    resi attivi i cerchi e uso frecce tastiera

--- main.js
+++ main.js
@@ -1,37 +1,72 @@
 //Generare un carosello di immagini
+$(function() {
+    //1. Prendo le icone delle frecce con jQuery e le metto in una variabile
+    var avanti = $('.next i');
+    var indietro = $('.prev i');
 
-//1. Prendo le icone delle frecce con jQuery e le metto in una variabile
+    //Generiamo un carosello in avanti spostando la classe active da una img all'altra
+    avanti.click(function() {
+        var imgVisualizzata = $('img.active');
+        imgVisualizzata.removeClass('active').next().addClass('active');
 
-var avanti = $('.next i');
+        if(imgVisualizzata.hasClass('last')) {
+            $('img.first').addClass('active');
+        }
+        
+        //Rendiamo attivi i cerchi
+        var cerchioBlu = $('i.active');
+        cerchioBlu.removeClass('active').next().addClass('active');
 
-var indietro = $('.prev i');
+        if(cerchioBlu.hasClass('last')) {
+            $('i.first').addClass('active');
+        }
+    });
 
-//Generiamo un carosello in avanti spostando la classe active da una img all'altra
+    //Ora facciamo in senso opposto
+    indietro.click(function() {
+        var imgVisualizzata = $('img.active');
+        imgVisualizzata.removeClass('active').prev().addClass('active');
 
-avanti.click(function() {
-    var imgVisualizzata = $('img.active');
+        if(imgVisualizzata.hasClass('first')) {
+            $('img.last').addClass('active');
+        }
 
-    imgVisualizzata.removeClass('active');
+        //Rendiamo i cerchi attivi
+        var cerchioBlu = $('i.active');
+        cerchioBlu.removeClass('active').prev().addClass('active');
 
-    imgVisualizzata.next().addClass('active');
+        if(cerchioBlu.hasClass('first')) {
+            $('i.last').addClass('active');
+        }
+        
+    });
 
-    if(imgVisualizzata.hasClass('last')) {
-        $('img.first').addClass('active');
-    }
-    
-});
+    //Implementiamo l'uso delle frecce della tastiera
+    $(document).keydown(function(x) {
+        var imgVisualizzata = $('img.active');
+        var cerchioBlu = $('i.active');
+        if (x.which == 37) {
+            imgVisualizzata.removeClass('active').prev().addClass('active');
+            cerchioBlu.removeClass('active').prev().addClass('active');
 
-//Ora facciamo in senso opposto
+            if(imgVisualizzata.hasClass('first')) {
+                $('img.last').addClass('active');
+            }
+            if(cerchioBlu.hasClass('first')) {
+                $('i.last').addClass('active');
+            }
 
-indietro.click(function() {
-    var imgVisualizzata = $('img.active');
+        } else if (x.which == 39) {
+            imgVisualizzata.removeClass('active').next().addClass('active');
+            cerchioBlu.removeClass('active').next().addClass('active');
 
-    imgVisualizzata.removeClass('active');
+            if(imgVisualizzata.hasClass('last')) {
+                $('img.first').addClass('active');
+            }
+            if(cerchioBlu.hasClass('last')) {
+                $('i.first').addClass('active');
+            }
 
-    imgVisualizzata.prev().addClass('active');
-
-    if(imgVisualizzata.hasClass('first')) {
-        $('img.last').addClass('active');
-    }
-    
-});
+        }
+    });
+})
